230152093039a136a0d628a0d32bed30d9b61ff0
Midnightgb <midnight3424@gmail.com>
2024-02-21 19:55:27 -0500

    Add new activities and layouts for authentication, home, profile, and user management

---
+++ 04-AppMovil/MainApp/app/src/main/java/com/example/parkingapp/ui/user/AddUserActivity.java
@@ -0,0 +1,16 @@
+package com.example.parkingapp.ui.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.parkingapp.R;
+
+public class AddUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_user);
+    }
+}
