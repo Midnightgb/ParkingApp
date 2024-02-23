ccbd2e3de35ae4cb9e8a680fc23cc07d011b4add
Luna717 <lunarave911@gmail.com>
2024-02-23 07:22:06 -0500

    activity create parking

---
+++ 04-AppMovil/MainApp/app/src/main/java/com/example/parkingapp/CreateParking2.java
@@ -0,0 +1,14 @@
+package com.example.parkingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateParking2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_parking2);
+    }
+}
