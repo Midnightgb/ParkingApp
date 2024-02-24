1ef3b23f990b7761e10b1886a597100323e0a248
Mhaccgg <herrerasalazar728@gmail.com>
2024-02-24 18:01:24 -0500

    Añadir color y cadena de texto en archivos XML y eliminar código comentado en archivo Java

--- 04-AppMovil/MainApp/app/src/main/java/com/example/parkingapp/ui/ticket/resume_ticket.java
+++ 04-AppMovil/MainApp/app/src/main/java/com/example/parkingapp/ui/ticket/resume_ticket.java
@@ -30,15 +30,6 @@ public class resume_ticket extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment resume_ticket.
-     */
-    // TODO: Rename and change types and number of parameters
     public static resume_ticket newInstance(String param1, String param2) {
         resume_ticket fragment = new resume_ticket();
         Bundle args = new Bundle();
