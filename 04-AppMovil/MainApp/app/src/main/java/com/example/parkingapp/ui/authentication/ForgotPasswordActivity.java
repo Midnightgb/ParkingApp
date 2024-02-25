ffc5f416bbe253729ef3e70beb7db6f91dc89e7e
Midnightgb <midnight3424@gmail.com>
2024-02-25 15:45:34 -0500

    Refactor forgot password UI and add email validation

--- 04-AppMovil/MainApp/app/src/main/java/com/example/parkingapp/ui/authentication/ForgotPasswordActivity.java
+++ 04-AppMovil/MainApp/app/src/main/java/com/example/parkingapp/ui/authentication/ForgotPasswordActivity.java
@@ -17,51 +17,55 @@ import com.example.parkingapp.MainActivity;
 import com.example.parkingapp.R;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
-    LinearLayout llEmail;//llEmail is visible
-    LinearLayout btnContinue;//btnContinue is visible
-    TextView textHelperEmail;//textHelperEmail is visible
+    LinearLayout sectionInputEmail;
+    LinearLayout llEmail;
+    LinearLayout btnSendCode;
     EditText etEmail;
-
     ImageView btnBack;
-
-    
     Context context;
-    
     //section code helper
-    LinearLayout llCode;//llCode is gone
-    LinearLayout textLlCode;//textLlCode is gone
-    LinearLayout btnConfirmCode;//btnConfirmCode is gone
-    LinearLayout llResendCode;
-
-    TextView tvUserEmail;
-    TextView timerResendCode;
-
-    EditText firstDigit;
-    EditText secondDigit;
-    EditText thirdDigit;
-    EditText fourthDigit;
-
+    LinearLayout sectionInputCode;
+    LinearLayout btnConfirmCode;//this is the confirm code button
+    LinearLayout llResendCode;//this is the resend code button
+    TextView tvUserEmail;//this is the user email displayed in the code helper
+    TextView timerResendCode;//this is the timer displayed in how much time left to be able to resend the code
+    EditText firstDigit;//When the user types the first digit of the code it will automatically focus on the second digit otherwise if the user deletes the first digit it will focus on the first digit
+    EditText secondDigit;//When the user types the second digit of the code it will automatically focus on the third digit otherwise if the user deletes the second digit it will focus on the first digit
+    EditText thirdDigit;//When the user types the third digit of the code it will automatically focus on the fourth digit otherwise if the user deletes the third digit it will focus on the second digit
+    EditText fourthDigit;//When the user types the fourth digit of the code it will enable the confirm code button otherwise if the user deletes the fourth digit it will focus on the third digit
     //onclick functions to add "sendCode" , "goBack", "confirmCode"
-
+    Boolean isEmailValid = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
-        llEmail = findViewById(R.id.llEmail);
-        btnBack = findViewById(R.id.btnBack);
-        etEmail = findViewById(R.id.etEmail);
-        btnContinue = findViewById(R.id.btnContinue);
         context = getApplicationContext();
-
-        btnContinue.setOnClickListener(v -> {
-            System.out.println("Email: " + etEmail.getText().toString());
-            Intent intention = new Intent(context, ResetPasswordActivity.class);
-            startActivity(intention);
+        sectionInputEmail = findViewById(R.id.sectionInputEmail);
+        sectionInputCode = findViewById(R.id.sectionInputCode);
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(v -> {
+            if (sectionInputCode.getVisibility() == View.VISIBLE) {
+                System.out.println("Go back to input email");
+                sectionInputCode.setVisibility(View.GONE);
+                sectionInputEmail.setVisibility(View.VISIBLE);
+            } else {
+                System.out.println("Go back to login");
+                Intent intent = new Intent(context, LoginActivity.class);
+                startActivity(intent);
+            }
         });
 
-        btnBack.setOnClickListener(v -> {
-            Intent intention = new Intent(context, LoginActivity.class);
-            startActivity(intention);
+        llEmail = findViewById(R.id.llEmail);
+        etEmail = findViewById(R.id.etEmail);
+
+        btnSendCode = findViewById(R.id.btnContinue);
+        btnSendCode.setOnClickListener(v -> {
+            if (isEmailValid) {
+                System.out.println("Sending code to email");
+                System.out.println("Email: " + etEmail.getText().toString());
+                sectionInputEmail.setVisibility(View.GONE);
+                sectionInputCode.setVisibility(View.VISIBLE);
+            }
         });
 
         etEmail.setOnFocusChangeListener((v, hasFocus) -> {
@@ -86,14 +90,17 @@ public class ForgotPasswordActivity extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
                 if (etEmail.getText().toString().isEmpty()) {
                     llEmail.setBackgroundResource(R.drawable.border_gray);
-                    btnContinue.setBackgroundResource(R.drawable.btn_inactive);
+                    btnSendCode.setBackgroundResource(R.drawable.btn_inactive);
+                    isEmailValid = false;
                 } else {
                     if (validateEmail(etEmail.getText().toString())) {
                         llEmail.setBackgroundResource(R.drawable.status_success);
-                        btnContinue.setBackgroundResource(R.drawable.btn_active);
+                        btnSendCode.setBackgroundResource(R.drawable.btn_active);
+                        isEmailValid = true;
                     } else {
-                        btnContinue.setBackgroundResource(R.drawable.btn_inactive);
+                        btnSendCode.setBackgroundResource(R.drawable.btn_inactive);
                         llEmail.setBackgroundResource(R.drawable.status_error);
+                        isEmailValid = false;
                     }
                 }
             }
