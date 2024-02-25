f05f9ed998cf4b66addf77724804dd184556d4f1
Midnightgb <midnight3424@gmail.com>
2024-02-24 19:39:38 -0500

    Refactor forgot password UI and update email string

--- 04-AppMovil/MainApp/app/src/main/java/com/example/parkingapp/ui/authentication/ForgotPasswordActivity.java
+++ 04-AppMovil/MainApp/app/src/main/java/com/example/parkingapp/ui/authentication/ForgotPasswordActivity.java
@@ -2,15 +2,75 @@ package com.example.parkingapp.ui.authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.example.parkingapp.MainActivity;
 import com.example.parkingapp.R;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+    LinearLayout llEmail;
+    ImageView btnBack;
+
+    EditText etEmail;
+
+    LinearLayout btnContinue;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        llEmail = findViewById(R.id.llEmail);
+        btnBack = findViewById(R.id.btnBack);
+        etEmail = findViewById(R.id.etEmail);
+        btnContinue = findViewById(R.id.btnContinue);
+        context = getApplicationContext();
+
+        btnContinue.setOnClickListener(v -> {
+            System.out.println("Email: " + etEmail.getText().toString());
+            Intent intention = new Intent(context, ResetPasswordActivity.class);
+            startActivity(intention);
+        });
+
+        btnBack.setOnClickListener(v -> {
+            Intent intention = new Intent(context, LoginActivity.class);
+            startActivity(intention);
+        });
+
+        etEmail.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                llEmail.setBackgroundResource(R.drawable.status_acti);
+            } else {
+                llEmail.setBackgroundResource(R.drawable.border_gray);
+            }
+        });
+        etEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (etEmail.getText().toString().isEmpty()) {
+                    llEmail.setBackgroundResource(R.drawable.status_error);
+                } else {
+                    llEmail.setBackgroundResource(R.drawable.status_acti);
+                }
+            }
+        });
     }
 }
