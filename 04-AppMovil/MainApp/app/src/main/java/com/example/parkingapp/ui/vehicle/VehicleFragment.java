2fe5498e9927c2917a4934b396d39df6ab429797
Mhaccgg <herrerasalazar728@gmail.com>
2024-02-26 08:43:57 -0500

    cambios vehiculos

--- 04-AppMovil/MainApp/app/src/main/java/com/example/parkingapp/ui/vehicle/VehicleFragment.java
+++ 04-AppMovil/MainApp/app/src/main/java/com/example/parkingapp/ui/vehicle/VehicleFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -34,6 +35,7 @@ public class VehicleFragment extends Fragment {
     Config dataConfig;
     RecyclerView recycler_view_vehicles;
     AdapterVehicles adapterVehicles;
+    Button btn_charge;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -42,18 +44,32 @@ public class VehicleFragment extends Fragment {
         View root = binding.getRoot();
         context = root.getContext();
         dataConfig = new Config(context);
+        btn_charge = binding.btnCharge;
         recycler_view_vehicles = binding.recyclerViewVehicles;
-        SharedPreferences sharedPreferences = context.getSharedPreferences("userParking", Context.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = context.getSharedPreferences("ticket", Context.MODE_PRIVATE);
 
-        String id = sharedPreferences.getString("id", null);
+        String userid = sharedPreferences.getString("id", null);
+        System.out.println("este es el id"+userid);
+        SharedPreferences id_ticket = context.getSharedPreferences("userParking", Context.MODE_PRIVATE);
+
+        String id = id_ticket.getString("id", null);
         System.out.println("este es el id"+id);
-        String userid =id;
+        String idticket =id;
         String url = dataConfig.getEndPoint("/ticket/getTicketRol.php?id="+userid);
         listaVehiculos = new JSONArray();
         adapterVehicles = new AdapterVehicles(listaVehiculos, binding.getRoot());
         consumirApi(url);
 
+        btn_charge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                String url = dataConfig.getEndPoint("/ticket/getTicket.php.php?id="+idticket);
+
+                buscarTicket(url);
+                switchSection(binding.layoutResumeTicket, binding.layoutTicket);
+            }
+        });
 
 
         return root;
@@ -112,5 +128,57 @@ public class VehicleFragment extends Fragment {
 
         queue.add(solicitud);
 
+    }
+
+    public  void buscarTicket (String url){
+        RequestQueue queue = Volley.newRequestQueue(context.getApplicationContext());
+
+        StringRequest solicitud =  new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String status = jsonObject.getString("status");
+                    System.out.println(jsonObject);
+                    String message = jsonObject.getString("message");;
+                    System.out.println(message);
+                    System.out.println(status);
+                    if(status.equals("true")){
+                        JSONObject ticket = jsonObject.getJSONObject("datos");
+                        binding.NameParking2.setText(ticket.getString("name_parking"));
+                        binding.addressParking2.setText(ticket.getString("address_parking"));
+                        binding.Plate_vehicle2.setText(ticket.getString("plate_vehicle"));
+                        binding.date_entry2.setText(ticket.getString("date_entry"));
+                        binding.total.setText(ticket.getString("duration"));
+
+
+
+                    }else{
+                        Toast.makeText(context.getApplicationContext(),message,Toast.LENGTH_LONG).show();
+                    }
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("El servidor GET responde con un error:");
+                System.out.println(error.getMessage());
+                Toast.makeText(context.getApplicationContext(),"Error al crear al usuario",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        queue.add(solicitud);
+    }
+    public void switchSection(View section, View previousSection) {
+        if (section.getVisibility() == View.GONE) {
+            section.setVisibility(View.VISIBLE);
+            previousSection.setVisibility(View.GONE);
+        } else {
+            section.setVisibility(View.GONE);
+        }
     }
 }
